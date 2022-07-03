84e1354183892c646a0ce5c449e12b0ddff12e4e
Igor Mascarenhas <igorsmascarenhas@outlook.com>
2022-07-03 15:20:36 -0300

    add first steps

---
+++ src/main/kotlin/com/igormascarenhas/Greeting.kt
@@ -0,0 +1,3 @@
+package com.igormascarenhas
+
+class Greeting(val id: Long, val content: String)
---
+++ src/main/kotlin/com/igormascarenhas/Startup.kt
@@ -0,0 +1,11 @@
+package com.igormascarenhas
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RestWithSpringBootAndKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<RestWithSpringBootAndKotlinApplication>(*args)
+}
